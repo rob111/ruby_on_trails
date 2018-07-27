7f23f5b9977b1a39ed403218dc6113558fe11d1f
Glen1337 <36278632+Glen1337@users.noreply.github.com>
2018-07-27 13:31:44 -0400

    Update users_controller.rb
    removed comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,6 @@ class UsersController < ApplicationController
     @user = User.find_by(username: params[:id])
     if @user.update(user_params)
       redirect_to @user, notice: 'User was successfully updated.'
-      # redirect_to "/users/#{@user.id}"
     else
       flash[:notice] = 'All fields must be completed'
       render :show
