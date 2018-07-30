c001a4088f979c7f01cb851898f95d0f9880882c
ezekielgiordano <40302800+ezekielgiordano@users.noreply.github.com>
2018-07-30 13:54:48 -0400

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,6 @@
 # user4 = User.create!(username: "ADMIN", email: 'admin@gmail.com', password: '111111', role: 'admin')
 
 
-trail1 = Trail.create(name: 'First Trail', street: '123 Maple St.', city: 'Boston', state: 'MA', zip: '12345', start_latitude: '1', start_longitude: '2', length: '6', difficulty: '7', elevation: '80')
-trail2 = Trail.create(name: 'Another Trail', street: '456 Elm St.', city: 'Springfield', state: 'IL', zip: '09876', start_latitude: '2', start_longitude: '4', length: '3', difficulty: '5', elevation: '45')
-trail3 = Trail.create(name: 'Third Trail', street: '789 Main St.', city: 'Atlanta', state: 'GA', zip: '54321', start_latitude: '3', start_longitude: '7', length: '9', difficulty: '4', elevation: '23')
+trail1 = Trail.create!(name: 'First Trail', street: '123 Maple St.', city: 'Boston', state: 'MA', zip: '12345', start_latitude: '1', start_longitude: '2', length: '6', difficulty: '7', elevation: '80')
+trail2 = Trail.create!(name: 'Another Trail', street: '456 Elm St.', city: 'Springfield', state: 'IL', zip: '09876', start_latitude: '2', start_longitude: '4', length: '3', difficulty: '5', elevation: '45')
+trail3 = Trail.create!(name: 'Third Trail', street: '789 Main St.', city: 'Atlanta', state: 'GA', zip: '54321', start_latitude: '3', start_longitude: '7', length: '9', difficulty: '4', elevation: '23')
